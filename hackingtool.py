6ebca1d142fbc441d9dd089c7696519248392ae8
Bakhtiyar Khan <86647424+Bakthayir@users.noreply.github.com>
2024-01-19 11:17:35 +0530

    Update hackingtool.py

--- hackingtool.py
+++ hackingtool.py
@@ -72,7 +72,7 @@ class AllTools(HackingToolsCollection):
 if __name__ == "__main__":
     try:
         if system() == 'Linux':
-            fpath = "/home/hackingtoolpath.txt"
+            fpath = "/home/BestKaliTools.txt"
             if not os.path.exists(fpath):
                 os.system('clear')
                 # run.menu()
